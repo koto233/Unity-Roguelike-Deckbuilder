4407fdffb576014d46406e6409a9636040c4e74a
koto <1768733431@qq.com>
2026-08-14 18:04:27 +0800

    feat(game-config): 更新卡牌配置和战斗UI
    - 将卡牌"猛击"重命名为"痛击"，调整伤害值从12降至8，并添加新的效果ID 5
    - 将卡牌"猛击+"重命名为"痛击+"，调整伤害值从16降至10，并添加新的效果ID 5
    - 更新卡牌图标名称为" bash"
    - 激活战斗界面中的UI元素，调整其位置和尺寸参数

--- Roguelike-Deckbuilder/Assets/Scripts/UI/UICardDisplay/Generated/UICardDisplay.Bind.g.cs
+++ Roguelike-Deckbuilder/Assets/Scripts/UI/UICardDisplay/Generated/UICardDisplay.Bind.g.cs
@@ -4,24 +4,37 @@
 //     请勿手动修改此文件，重新生成将覆盖所有改动
 //
 //     来源 UI : UICardDisplay
-//     生成时间 : 2026-07-18
+//     生成时间 : 2026-08-14
 // </自动生成>
 //------------------------------------------------------------------------------
 
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
     public partial class UICardDisplay
     {
+        private @Image b_Icon;
+        private @Image b_Frame;
+        private @Image b_PortraitBorder;
+        private @Image b_Banner;
+        private @TextMeshProUGUI b_type_text;
         private @TextMeshProUGUI b_CostText;
         private @TextMeshProUGUI b_NameText;
         private @TextMeshProUGUI b_DescText;
+        private @RectTransform b_ArrowStartRoot;
 
         protected override void GetUI()
         {
             base.GetUI();
-            b_CostText = GetBind<@TextMeshProUGUI>(0);
-            b_NameText = GetBind<@TextMeshProUGUI>(1);
-            b_DescText = GetBind<@TextMeshProUGUI>(2);
+            b_Icon = GetBind<@Image>(0);
+            b_Frame = GetBind<@Image>(1);
+            b_PortraitBorder = GetBind<@Image>(2);
+            b_Banner = GetBind<@Image>(3);
+            b_type_text = GetBind<@TextMeshProUGUI>(4);
+            b_CostText = GetBind<@TextMeshProUGUI>(5);
+            b_NameText = GetBind<@TextMeshProUGUI>(6);
+            b_DescText = GetBind<@TextMeshProUGUI>(7);
+            b_ArrowStartRoot = GetBind<@RectTransform>(8);
         }
     }
